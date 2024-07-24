271d7c1eadf9cd43b4e7684c5da59abad8aca50f
alirezagguu <122180893+alirezagguu@users.noreply.github.com>
2024-07-25 03:01:26 +0330

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('myButton')
+btn.addEventListener('click',
+    (event) => {
+        console.log('Alireza');
+    }
+)
+
+const namee = document.getElementById('name').onclick = () => console.log('hello');
