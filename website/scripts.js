adbd0de83a041e21e41dbaf11e05a3776fe8f7b2
alirezagguu <122180893+alirezagguu@users.noreply.github.com>
2024-07-25 03:01:56 +0330

    Delete website directory

--- website/scripts.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById('myButton')
-btn.addEventListener('click',
-    (event) => {
-        console.log('Alireza');
-    }
-)
-
-const namee = document.getElementById('name').onclick = () => console.log('hello');
